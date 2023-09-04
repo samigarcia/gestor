aabaf0a0f31ebdee3cd04cbc5400787593ac00e5
JavierDuranFlores <duranfloresj7@gmail.com>
2023-09-04 09:51:26 -0600

    mejora-de-intrefaz-grafica

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -21,34 +21,91 @@ class _MyAppFormState extends State<MyAppForm> {
         children: <Widget>[
           Column(
             children: [
-              const CircleAvatar(
-                radius: 100.0,
-                backgroundColor: Colors.blueAccent,
-                child: Text(
-                  "Registro",
-                  style: TextStyle(
-                    fontFamily: 'Times New Roman',
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+              Align(
+                alignment: const AlignmentDirectional(0, 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 300,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF6BA0E3), Color(0xFF2874CF)],
+                          stops: [0, 1],
+                          begin: AlignmentDirectional(0.1, -1),
+                          end: AlignmentDirectional(-0.1, 1),
+                        ),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(400),
+                          bottomRight: Radius.circular(70),
+                          topLeft: Radius.circular(0),
+                          topRight: Radius.circular(0),
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: const AlignmentDirectional(-1, 0.14),
+                            child: Container(
+                                width: 110,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF485876),
+                                  borderRadius: const BorderRadius.only(
+                                    bottomLeft: Radius.circular(70),
+                                    bottomRight: Radius.circular(70),
+                                    topLeft: Radius.circular(0),
+                                    topRight: Radius.circular(70),
+                                  ),
+                                  shape: BoxShape.rectangle,
+                                  border: Border.all(
+                                    color: const Color(0xFF485876),
+                                  ),
+                                ),
+                                child: const Align(
+                                  alignment: AlignmentDirectional(-0.93, -0.65),
+                                  child: Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        8, 8, 8, 8),
+                                    child: Icon(
+                                      Icons.admin_panel_settings_outlined,
+                                      color: Color(0xFF0096F9),
+                                      size: 60,
+                                    ),
+                                  ),
+                                )),
+                          ),
+                          const Align(
+                            alignment: AlignmentDirectional(0.14, 0.07),
+                            child: Text(
+                              'Registro',
+                              style: TextStyle(
+                                fontSize: 40.0,
+                                color: Colors.white,
+                                fontFamily: 'Roboto',
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: const AlignmentDirectional(-1.00, 0.80),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: const StadiumBorder(),
+                                backgroundColor:
+                                    const Color.fromARGB(255, 43, 73, 245),
+                              ),
+                              child: const Text('Guardar'),
+                              onPressed: () {
+                                debugPrint('Click Guardado');
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 1, right: 265),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    backgroundColor: const Color.fromARGB(255, 43, 73, 245),
-                  ),
-                  child: const Text('Guardar'),
-                  onPressed: () {
-                    debugPrint('Click Guardado');
-                  },
-                ),
-              ),
-              const Divider(
-                height: 15.0,
               ),
               Container(
                 margin: const EdgeInsets.only(right: 280),
@@ -231,4 +288,3 @@ class _MyAppFormState extends State<MyAppForm> {
     );
   }
 }
-//fin
